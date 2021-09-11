9224693f6ca5b3e452cafb49859e7a174bf41832
wdesimini <wdesimini8@gmail.com>
2021-09-11 09:26:19 -0700

    store user id locally

---
+++ HabitTracker/Data/UserDefaults+Service.swift
@@ -0,0 +1,34 @@
+//
+//  UserDefaults+Service.swift
+//  HabitTracker
+//
+//  Created by Wilson Desimini on 9/11/21.
+//
+
+import Foundation
+
+extension UserDefaults {
+    private enum Key: String {
+        case userId
+    }
+    
+    private func object<T: Decodable>(key: Key) -> T? {
+        data(forKey: key.rawValue).flatMap {
+            try? JSONDecoder().decode(T.self, from: $0)
+        }
+    }
+    
+    private func set<T: Encodable>(object: T, key: Key) throws {
+        let data = try JSONEncoder().encode(object)
+        setValue(data, forKey: key.rawValue)
+    }
+    
+    var userId: UUID? {
+        let idString: String? = object(key: .userId)
+        return idString.flatMap { UUID(uuidString: $0) }
+    }
+    
+    func update(userId: UUID) throws {
+        try set(object: userId.uuidString, key: .userId)
+    }
+}
